97c413f4ff42e5b986090f76d4ee720b4a79a178
asefibrahim <strongman01666@gmail.com>
2023-06-08 23:03:01 +0600

    Cart of classes added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
@@ -24,13 +24,54 @@ const client = new MongoClient(uri, {
     }
 });
 
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
         const userCollection = client.db('magicdb').collection('user')
+        const classCollection = client.db('magicdb').collection('classColloection')
 
+        const instructorCollection = client.db('magicdb').collection('InstructorCollection')
+        const cartCollection = client.db('magicdb').collection('cart')
+
+
+
+        // carts related Api
+        app.post('/carts', async (req, res) => {
+            try {
+                const selectedUser = req.body;
+                selectedUser._id = new ObjectId(); // Generate a new unique _id value
+                const result = await cartCollection.insertOne(selectedUser);
+                res.send(result);
+            } catch (error) {
+                if (error.code === 11000) {
+                    res.status(400).send("Duplicate key error: _id must be unique.");
+                } else {
+                    res.status(500).send("Internal Server Error");
+                }
+            }
+        });
+
+
+        // InstructorRelated API
+        app.get('/instructors', async (req, res) => {
+            const result = await instructorCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        // Class Related API
+
+        app.get('/classes', async (req, res) => {
+            const result = await classCollection.find().toArray()
+            res.send(result)
+        })
+
+
+
+        // user related API
         app.post('/user', async (req, res) => {
             const user = req.body
             const result = await userCollection.insertOne(user)
